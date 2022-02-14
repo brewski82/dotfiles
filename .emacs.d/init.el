838b81f4adf59e8da216ca441b7db85627f1c61e
William Bruschi <william.bruschi@gmail.com>
2022-02-14 08:51:33 -0500

    Helm find files.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,7 +18,7 @@
 (straight-use-package 'helm)
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-;; (global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
 (helm-mode 1)
 
 (require 'cl-lib)
